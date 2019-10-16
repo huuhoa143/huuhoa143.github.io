91a0948c4bb08f9fc6c518352da356bd4d8606fa
Hòa Nguyễn Hữu <huuhoa14399@gmail.com>
2019-10-16 20:35:09 +0700

    demo ci

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,7 +21,7 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'A hi hi', // Title for your website.
+  title: 'A hi hi he he', // Title for your website.
   tagline: 'Datagrin Test',
   url: 'https://huuhoa143.github.io', // Your website URL
   baseUrl: '/huuhoa143.github.io/', // Base URL for your project */
